9688eb72ba43d5f30f69dd7417577ebe2290e1f7
nyagaderrick <nyagad@icloud.com>
2024-01-06 18:54:48 +0300

    Setting up Svelte

---
+++ Film_project/src/routes/films/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    let film = 'The Godfather'
+</script>
+
+<!-- <h1>{film.toUpperCase()}</h1>
+
+<style>
+    h1 {
+        font-size: 40px;
+        color: purple;
+        position: center;
+        text-align: center;
+        margin-top: 100px;
+
+    }
+ </style>    -->
+
+{#if film.startsWith('f')}
+    <p>It starts with 'T'</p>
+{:else}
+    <p>It doesn't start with 'T'</p>
+{/if}
